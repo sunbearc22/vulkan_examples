40b6c146e223b956d1466ea4c52047a9f002c374
sunbearc22 <sunbear.c22@gmail.com>
2017-10-18 18:01:52 +0800

    Working files

---
+++ HelloTriangle/App_v2_withDebugCallback.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+"""
+vulkan app to draw a 3D image
+"""
+__author__ = 'sunbearc22'
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+# Python3 modules
+import logging
+
+# API
+from vulkan import vkDeviceWaitIdle
+
+# Application Modules
+import sdl2window_v2__withDebugCallback as sw
+import vulkanbase_v2_withDebugCallback as vb
+
+###############################################################################
+# Global variables
+###############################################################################
+TITLE = 'HelloTriangle'
+WIDTH = 400
+HEIGHT = 400
+LOGFORMAT = '%(asctime)s [%(process)d] %(name)s %(module)s.%(funcName)-33s'\
+            '+%(lineno)-5s: %(levelname)-8s %(message)s'
+
+logging.basicConfig(level=logging.DEBUG, format=LOGFORMAT)
+#logging.basicConfig(level=logging.INFO, format=LOGFORMAT)
+
+class VulkanApp(object):
+
+    def __init__(self):
+        self.vulkan_window = None
+        
+        self._initWindow()
+        self._initVulkan(self.vulkan_window);
+        self._mainLoop();
+
+    def _initWindow(self):
+        self.vulkan_window = sw.SetWindow(title=TITLE, w=WIDTH, h=HEIGHT)
+
+    def _initVulkan(self, window):
+        self.vulkan_base = vb.Setup(window, debug=True)
+        print("self.vulkan_base =", self.vulkan_base)
+
+    def _mainLoop(self):
+        # Main loop
+        running = True
+        logging.info('Executing Draw Loop.')
+        while running:
+            events = sw.sdl2.ext.get_events()
+            self.vulkan_base._drawFrame()
+            for event in events:
+                if event.type == sw.sdl2.SDL_QUIT:
+                    logging.info('Leaving Draw Loop: a sdl2 event was detected.')
+                    running = False
+                    vkDeviceWaitIdle( self.vulkan_base.logical_device )
+                    logging.info('Checked all outstanding queue operations for all'
+                                 ' queues in Logical Device have ceased.')
+                    break
+
+def main():
+    app = VulkanApp()
+    app.vulkan_base.cleanup()
+    app.vulkan_window.destroy()
+    print("app =", app)
+    
+
+if __name__ == "__main__":
+    main()
+    print("main =", main)
